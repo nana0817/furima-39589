64e08e1b0c6f0a78beab38e77b7287e71607bb0a
nana0817 <siebenwmf@gmail.com>
2023-10-18 17:21:21 +0900

    Userモデルのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :chinese_character_family_name, presence: true
+  validates :chinese_character_first_name, presence: true
+  validates :katakana_family_name, presence: true
+  validates :katakana_first_name, presence: true
+  validates :date_of_birth, presence: true
 end
