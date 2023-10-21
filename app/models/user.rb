0f73fcf987312a12e2b969b9dd5e4acb9f45cd51
nana0817 <siebenwmf@gmail.com>
2023-10-21 18:22:15 +0900

    バリデーションの追加・修正
    パスワードのバリデーションを正規表現を用いて追加
    
    また、フルネームのバリデーションに不備があったので、それを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,26 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname,        presence: true, length: { maximum: 40 }
+  # ニックネームのバリデーション
+  validates :nickname, presence: true, length: { maximum: 40 }
+
+  # パスワードのバリデーション
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には半角英数字両方を含めて設定してください' }
+
   # フルネーム(全角)のバリデーション
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角(漢字・ひらがな・カタカナ)のみが使えます' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字(漢字・ひらがな・カタカナ)を使用してください' } do
     validates :last_name
     validates :first_name
   end
+
   # フルネーム(全角カナ)のバリデーション
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角(カタカナ)のみが使えます' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角文字(カタカナ)を使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
-  
-  validates :date_of_birth,   presence: true
+
+# 生年月日のバリデーション
+  validates :date_of_birth, presence: true
+
 end
