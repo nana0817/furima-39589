9bd2e1e0b0e836183b31fc61fd245741437b50b9
nana0817 <siebenwmf@gmail.com>
2023-10-21 12:09:11 +0900

    バリデーションを追加
    以下のバリデーションを新たに追加
    last_name, first_name
    ↓
    正規表現を用いて、全角(漢字・ひらがな・カタカナ)のみ使えるようにした
    
    last_name_kana, first_name_kana
    ↓
    正規表現を用いて、全角(カタカナ)のみ使えるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :nickname,        presence: true, length: { maximum: 40 }
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
+  # フルネーム(全角)のバリデーション
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角(漢字・ひらがな・カタカナ)のみが使えます' } do
+    validates :last_name
+    validates :first_name
+  end
+  # フルネーム(全角カナ)のバリデーション
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角(カタカナ)のみが使えます' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  
   validates :date_of_birth,   presence: true
 end
