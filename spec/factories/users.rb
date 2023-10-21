b9b25bea0866611fe974ebc692222a0a2f14e3f0
nana0817 <siebenwmf@gmail.com>
2023-10-21 12:33:31 +0900

    カラム名の修正
    family_name → last_name
    family_name_kana → last_name_kana
    と修正するのを忘れていた。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     gimei = Gimei.name
-    family_name           {gimei.last.kanji}
+    last_name           {gimei.last.kanji}
     first_name            {gimei.first.kanji}
-    family_name_kana      {gimei.last.katakana}
+    last_name_kana      {gimei.last.katakana}
     first_name_kana       {gimei.first.katakana}
     date_of_birth         {Faker::Date.between(from: '1930-1-1', to: Date.today)}
   end
