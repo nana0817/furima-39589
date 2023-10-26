c0a48c2203025e0df11ef27e23874e54437b9e84
nana0817 <siebenwmf@gmail.com>
2023-10-26 12:18:36 +0900

    ActiveHashの導入
    以下を実行
    ・必要なGem(active_hash)をインストール
    ・app/modelsディレクトリに「category.rb」「condiion.rb」「shipping_fee.rb」「prefecture.rb」「shipping_time.rb」ファイルを作成
    ・作成したファイルに必要な記述を追加
    ・作成したファイルそれぞれにItemモデルとのアソシエーションを記述
    ・app/models/item.rbファイルに作成したファイルとのアソシエーションを記述
    ・app/models/item.rbファイルにバリデーションを追加
    ・app/views/items/new.html.erbファイルにActiveHashのための記述を追加
    ・app/views/items/new.html.erbファイルのname属性がhogeとなっていたところを適切な値に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,19 +4,23 @@ class Item < ApplicationRecord
 
   validates :item_description, presence: true  # 商品の説明
 
-  validates :category_id,      presence: true  # カテゴリー
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id       # カテゴリー
+    
+    validates :condition_id      # 商品の状態
 
-  validates :condition_id,     presence: true  # 商品の状態
+    validates :shipping_fee_id   # 配送料の負担
 
-  validates :shipping_fee_id,  presence: true  # 配送料の負担
+    validates :prefecture_id     # 発送元の地域
 
-  validates :prefecture_id,    presence: true  # 発送元の地域
-
-  validates :shipping_time_id, presence: true  # 発送までの日数
+    validates :shipping_time_id  # 発送までの日数
+  end
 
   validates :price,            presence: true, length: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/, message: 'には半角数字を使用してください' }  # 価格
 
+
   # アソシエーション
   belongs_to :user
+  belongs_to_active_hash :category, :condition, :shipping_fee, :prefecture, :shipping_time
 
 end
