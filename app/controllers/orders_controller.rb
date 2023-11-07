c35823106afb46f5ea4aa4cfdf978f4281e384f6
nana0817 <siebenwmf@gmail.com>
2023-11-07 19:25:05 +0900

    購入機能に必要なモデルやコントローラーを作成
    以下を実行
    ・Orderモデルを作成
    ・Addressモデルを作成
    ・OrderモデルとAddressモデルのアソシエーションを定義
    ・OrderモデルとItemモデルのアソシエーションを定義
    ・ordersコントローラーを作成
    ・config/routes.rbにて、ordersのルーティングを記述(itemsのルーティングにネスト)
    ・ordersコントローラーにFormオブジェクトのインスタンスを生成する処理を記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,22 @@
+class OrdersController < ApplicationController
+  def index
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, 
+                                          :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
