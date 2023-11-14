85e456eda1f5959a865e280d16ac56212d3c6483
nana0817 <siebenwmf@gmail.com>
2023-11-14 12:18:02 +0900

    Javascriptファイル読み込みの不具合を解消

--- app/javascript/calculate.js
+++ app/javascript/calculate.js
@@ -1,15 +1,18 @@
-const priceInput = document.getElementById("item-price");
-const taxPrice = document.getElementById("add-tax-price");
-const profit = document.getElementById("profit");
+window.addEventListener("turbo:load", () => {
+  const priceInput = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
 
-priceInput.addEventListener("input", () => {
-  // 「価格」フォームに入力した値を取得
-  const inputValue = priceInput.value;
+  priceInput.addEventListener("input", () => {
+    // 「価格」フォームに入力した値を取得
+    const inputValue = priceInput.value;
 
-  // 「販売手数料」に表示する値
-  taxPrice.innerHTML = Math.floor(inputValue * 0.1);
+    // 「販売手数料」に表示する値
+    taxPrice.innerHTML = Math.floor(inputValue * 0.1);
 
-  // 「販売利益」に表示する値
-  profit.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+    // 「販売利益」に表示する値
+    profit.innerHTML = Math.floor(inputValue - inputValue * 0.1);
+
+  });
 
 });
